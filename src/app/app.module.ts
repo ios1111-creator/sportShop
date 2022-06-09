1e9448677a884e96da296039631a16bf113b8bbf
Pawlik <pawlik.addam@gmail.com>
2022-06-09 15:39:45 +0200

    Create new project sportShop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SportStoreComponent } from './sport-store/sport-store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SportStoreComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sport-store/sport-store.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SportStoreComponent } from './sport-store.component';
+
+describe('SportStoreComponent', () => {
+  let component: SportStoreComponent;
+  let fixture: ComponentFixture<SportStoreComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SportStoreComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SportStoreComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
